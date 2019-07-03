c2e3a1e5a705339dc61fb210779bb226066e1298
Niels Buekers <niels.buekers@gmail.com>
2019-07-03 18:33:50 +0200

    change to team + add clean up

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-PROJECT=training-190916
+PROJECT=my-gke-rbac-sandbox
 ZONE=europe-west1-d
 CLUSTER_NAME=gke-rbac
-DEV_GROUP=gke-rbac-users@fourcast.io
 ROLE_NAME=GKE_authenticate_users
+DEV_GROUP=team_1@fourcast.io
+GKE_DOMAIN_GROUP=gke-security-groups@fourcast.io
 
 #Creating the appropriate least-privileged IAM role"
 echo "### Step 1 - Creating IAM role"
@@ -13,13 +13,15 @@ gcloud iam roles create $ROLE_NAME --file iam.yaml --project $PROJECT
 # Grant the above IAM role to our developer group
 # It is a security best practice to create a Group and grant the IAM permissions to this Group.
 # Then add members to this group to inherit these permissions.
+# For more info on this, please refer to https://cloud.google.com/kubernetes-engine/docs/how-to/role-based-access-control#groups-setup-gsuite.
+# Don't forget to create gke-security-groups@[yourdomain.com] and add your RBAC group here
 echo "\n### Step 2 - Granting IAM role"
 gcloud projects add-iam-policy-binding $PROJECT --member group:$DEV_GROUP --role projects/$PROJECT/roles/$ROLE_NAME
 
 # Create a brand new GKE cluster
 # As of v1.12, Legacy ABAC is disabled by default, which we need for RBAC to function.
 echo "\n### Step 3 - Creating GKE cluster"
-gcloud container clusters create $CLUSTER_NAME --project $PROJECT --zone $ZONE
+gcloud beta container clusters create $CLUSTER_NAME --project $PROJECT --zone $ZONE --security-group=$GKE_DOMAIN_GROUP
 
 # Use gcloud to populate your ~/.kube/config. This will add the cluster and your gcloud user and combine them in a kubectl context.
 echo "\n### Step 4 - Get admin credentials to cluster"
@@ -38,3 +40,8 @@ echo "gcloud container clusters get-credentials ${CLUSTER_NAME} --project ${PROJ
 
 echo "\n### Step 7"
 echo "There is no step 7. Time for a good Belgian beer\!"
+
+read -p "Press enter to clean up"
+
+gcloud container clusters delete $CLUSTER_NAME --project $PROJECT --zone $ZONE
+gcloud iam roles delete $ROLE_NAME --project $PROJECT
